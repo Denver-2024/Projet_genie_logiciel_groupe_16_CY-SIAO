5d17c55f8b8707c1dd5b8e873075671cab2ae317
KHELIL Yacine <133356569+NaTwiX27@users.noreply.github.com>
2025-05-13 19:03:18 +0200

    Création de Room

--- src/main/java/com/cy_siao/model/Room.java
+++ src/main/java/com/cy_siao/model/Room.java
@@ -1,14 +1,26 @@
 package com.cy_siao.model;
 
+import com.cy_siao.model.RestrictionType;
 import com.cy_siao.model.person.Person;
 
-import java.io.*;
-import java.util.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 
 /**
- * 
+ * Represents a room that contains beds and applies restrictions.
+ * Allows management of beds, restrictions, and compatibility checks.
  */
 public class Room {
+
+    private int id;
+    private String name;
+    private int nbBedsMax;
+
+    private List<Bed> beds = new ArrayList<>();
+    private List<RestrictionType> restrictions = new ArrayList<>();
 
     /**
      * Default constructor
@@ -17,68 +29,124 @@ public class Room {
     }
 
     /**
-     * 
+     * Constructor with parameters
      */
-    private int id;
+    public Room(int id, String name, int nbBedsMax) {
+        this.id = id;
+        this.name = name;
+        this.nbBedsMax = nbBedsMax;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNbBedsMax() {
+        return nbBedsMax;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNbBedsMax(int nbBedsMax) {
+        this.nbBedsMax = nbBedsMax;
+    }
+
+    public List<Bed> getBeds() {
+        return beds;
+    }
+
+    public List<RestrictionType> getRestrictions() {
+        return restrictions;
+    }
 
     /**
-     * 
-     */
-    private String name;
-
-    /**
-     * 
-     */
-    private int nbBedsMax;
-
-    /**
-     * 
-     */
-    private RestrictionType restrictionType;
-
-
-
-
-
-    /**
-     * @param bed 
-     * @return
+     * Adds a bed to the room if there's space.
      */
     public void addBed(Bed bed) {
-        // TODO implement here
+        if (beds.size() < nbBedsMax) {
+            beds.add(bed);
+        } else {
+            throw new IllegalStateException("Maximum number of beds reached for this room.");
+        }
     }
 
     /**
-     * @param bed 
-     * @return
+     * Removes a bed from the room.
      */
     public void removeBed(Bed bed) {
-        // TODO implement here
+        beds.remove(bed);
     }
 
     /**
-     * @param r 
-     * @return
+     * Adds a restriction to the room.
      */
-    public void addRestriction(RestrictionType r) {
-        // TODO implement here
+    public void addRestriction(RestrictionType restriction) {
+        if (!restrictions.contains(restriction)) {
+            restrictions.add(restriction);
+        }
     }
 
     /**
-     * @param r 
-     * @return
+     * Removes a restriction from the room.
      */
-    public void removeRestriction(RestrictionType r) {
-        // TODO implement here
+    public void removeRestriction(RestrictionType restriction) {
+        restrictions.remove(restriction);
     }
 
     /**
-     * @param person 
-     * @return
+     * Checks if a person respects all the room's restrictions.
      */
     public boolean isCompatible(Person person) {
-        // TODO implement here
-        return false;
+        for (RestrictionType restriction : restrictions) {
+            if (!restriction.isRespectedBy(person)) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    /**
+     * Returns the list of available beds for a given date range.
+     */
+    public List<Bed> getAvailableBeds(LocalDate dateArrival, LocalDate dateDeparture) {
+        List<Bed> availableBeds = new ArrayList<>();
+        for (Bed bed : beds) {
+            if (bed.isAvailable(dateArrival, dateDeparture)) {
+                availableBeds.add(bed);
+            }
+        }
+        return availableBeds;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", nbBedsMax=" + nbBedsMax +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Room room)) return false;
+        return id == room.id &&
+                nbBedsMax == room.nbBedsMax &&
+                Objects.equals(name, room.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, nbBedsMax);
+    }
 }

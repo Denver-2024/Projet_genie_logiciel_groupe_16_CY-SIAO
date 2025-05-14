b5db68248a5bb72b78ab2ac53b3c5d9ed294ce61
davekg8 <davidegbakou@gmail.com>
2025-05-14 23:13:05 +0200

    Update StayController to include CLI options menu

--- src/main/java/com/cy_siao/controller/StayController.java
+++ src/main/java/com/cy_siao/controller/StayController.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class StayController {
     public void start(Scanner scanner) {
         System.out.println("Welcome to the stay management");
-        System.out.println("Good luck!");
-        System.out.println("Flemme d'ecrire en anglais. Maintenance en cours");
+        System.out.println("Please select an option:");
+        System.out.println("1. Show free beds");
+        System.out.println("2. Show current stay");
+        System.out.println("0. Return");
     }
 }

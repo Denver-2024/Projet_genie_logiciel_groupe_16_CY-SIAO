8484fcadcc2a305f2cf27c9f2c7618aa42e74e63
Denver-2024 <schmidthumpich2023@gmail.com>
2025-05-20 20:44:18 +0200

    Some modifs

--- src/main/java/com/cy_siao/App.java
+++ src/main/java/com/cy_siao/App.java
@@ -3,21 +3,37 @@ package com.cy_siao;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/bed_view.fxml"));
-        Parent root = loader.load();
+    public void start(Stage primaryStage) {
+        //Relative path from resources for  adding the icon to our software
+        InputStream stream = getClass().getResourceAsStream("/Images/Projet_SIAO.png");
+        try {
+            if (stream != null) {
+                primaryStage.getIcons().add(new Image(stream));
+            }
+        } catch (Exception e) {
+            System.err.println("Error in loading image: " + e.getMessage());
+        }
 
-        Scene scene = new Scene(root);
-
-        primaryStage.setTitle("Gestion des personnes");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        primaryStage.setTitle("CY SIAO Application");
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/mainMenu.fxml")));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println("Error in loading the scene: "+e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
